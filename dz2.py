c1e9bf4eff606c18e283fcc3abcaffc28c36e449
mastergoroch <lubitelgoroha1811@mail.ru>
2024-07-25 13:34:50 +0200

    Update dz2.py

--- dz2.py
+++ dz2.py
@@ -1,2 +1,2 @@
-def ignore_command(command): return True if command in ['alias', 'configuration','ip','sql', 'delect', 'update', 'exec', 'del', 'trincate'] else False
+ignore_command= lambda command: True if command in ['alias', 'configuration','ip','sql', 'delect', 'update', 'exec', 'del', 'trincate'] else False
 print(ignore_command('alias'))
